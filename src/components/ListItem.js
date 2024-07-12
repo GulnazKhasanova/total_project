a82aa854e801a3e6c185b205d22b449fdec00e5a
Gulnaz <viteewato@mail.ru>
2024-07-13 01:02:26 +0300

    2

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,6 @@ import { DemoButton } from './Demo/DemoButton';
 
 export class ListItem extends Component {
   setup(props) {
-    console.log(props.item)
     const removeButton= new DemoButton({ text: 'Удалить', className: 'delete-button', onClick: this.removeItem.bind(this) });  
     this.$rootElement = document.createElement('div');
     this.$rootElement.className = 'donate-item';
